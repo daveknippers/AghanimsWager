72a24d49cf400f2178da36029618f235f47d87d7
Dave Knippers <dave.knippers@protonmail.com>
2022-12-02 21:18:40 -0700

    cleaned up the database code a lot, but it's still terrible.

--- DotaBet_discord.py
+++ DotaBet_discord.py
@@ -30,7 +30,6 @@ COMM_CHANNEL = 'dota-bet'
 
 pgdb = PGDB(CONNECTION_STRING,'DotaBet_discord')
 
-navy_seals = '''What the fuck did you just fucking say about me, you little bitch? I'll have you know I graduated top of my class in the Navy Seals, and I've been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I'm the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You're fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that's just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little "clever" comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn't, you didn't, and now you're paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You're fucking dead, kiddo.'''
 
 with open('heroes.json') as f:
 	hero_data = json.load(f)
@@ -217,7 +216,6 @@ class Lobby:
 		self.client = client
 		self.match_id = None
 		self.last_update = None
-
 
 		self.comm_channel = comm_channel
 
@@ -599,11 +597,11 @@ async def last(ctx,x_matches='10',user_id=None):
 	try:
 		x_matches = int(x_matches)
 		if x_matches < 1:
-			for msg in format_long(navy_seals):
+			for msg in format_long('>:('):
 				await ctx.send(msg)
 				return
 		if x_matches > 50:
-			for msg in format_long(navy_seals):
+			for msg in format_long('>:('):
 				await ctx.send(msg)
 				return
 	except ValueError:
@@ -693,7 +691,7 @@ async def feederboard(ctx,*arg):
 
 	db_result = pgdb.feederboard()
 	agg = []
-	agg.append('Rank  | Deaths [last 10] | Name ')
+	agg.append('Rank  | Deaths [last 20] | Name ')
 	for i,(discord_id,amount) in enumerate(db_result):
 		user = await bot.cached_user(discord_id)
 		agg.append('{:5d} | {:16d} | {}'.format(i+1,amount,user.name))
