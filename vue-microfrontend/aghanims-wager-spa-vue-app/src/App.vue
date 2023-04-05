e1aa79c2980c93e0811a7c833a447b671d0ce8c5
lavalle.nick <nlavshop@gmail.com>
2023-04-05 00:53:40 -0600

    getting rid of the users thing, FOR NOW

--- vue-microfrontend/aghanims-wager-spa-vue-app/src/App.vue
+++ vue-microfrontend/aghanims-wager-spa-vue-app/src/App.vue
@@ -20,14 +20,14 @@
             <b-dropdown-item href="#">RU</b-dropdown-item>
             <b-dropdown-item href="#">FA</b-dropdown-item>
           </b-nav-item-dropdown> -->
-          <b-nav-item-dropdown right>
+          <!-- <b-nav-item-dropdown right> -->
             <!-- Using 'button-content' slot -->
-            <template v-slot:button-content>
-              <em>User</em>
-            </template>
-            <b-dropdown-item href="#">Profile</b-dropdown-item>
-            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-          </b-nav-item-dropdown>
+            <!-- <template v-slot:button-content> -->
+              <!-- <em>User</em> -->
+            <!-- </template> -->
+            <!-- <b-dropdown-item href="#">Profile</b-dropdown-item> -->
+            <!-- <b-dropdown-item href="#">Sign Out</b-dropdown-item> -->
+          <!-- </b-nav-item-dropdown> -->
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
